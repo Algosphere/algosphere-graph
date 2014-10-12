a69dd12fe3e1cc865c259b1ff35cd8525b64acfe
dionisos <denis.baudouin@gmail.com>
2014-10-12 16:41:24 +0200

    pass argument gestion of xml_to_graphviz.py to argparse

--- xml_to_graphviz.py
+++ xml_to_graphviz.py
@@ -4,15 +4,18 @@
 # dot -Tsvg ci.dot > ci.svg
 
 import sys
+import argparse
 from CI_list import CI_list
 
-if(len(sys.argv) != 3):
-    print("xml_to_graphviz.py ci.xml ci.dot")
-    exit(1)
+parser = argparse.ArgumentParser(description='Create a graph of ci in a dot file(graphviz).')
+parser.add_argument('input_file', help='the xml file of CI')
+parser.add_argument('output_file', help='the name of the dot file produced')
+
+args = parser.parse_args()
 
 ci_list = CI_list([])
-ci_list.load_xml(sys.argv[1])
-a_file = open(sys.argv[2], 'w')
+ci_list.load_xml(args.input_file)
+a_file = open(args.output_file, 'w')
 
 
 a_file.write(ci_list.to_graphviz())
